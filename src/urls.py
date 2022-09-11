2201c55516f8fd7485157ca51273c2a788e04d46
Tang <mrgaoyi@gmail.com>
2022-09-11 17:48:06 +0700

    use djangorestframework-simplejwt and dj-rest-auth

--- src/urls.py
+++ src/urls.py
@@ -1,9 +1,9 @@
 from allauth.socialaccount.providers.facebook.views import \
     FacebookOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from dj_rest_auth.registration.views import SocialLoginView
 from django.contrib import admin
 from django.urls import include, path
-from rest_auth.registration.views import SocialLoginView
 
 
 class GoogleLogin(SocialLoginView):
@@ -16,8 +16,8 @@ class FacebookLogin(SocialLoginView):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('rest_auth.urls')),
-    path('api/auth/register/', include('rest_auth.registration.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/register/', include('dj_rest_auth.registration.urls')),
     path('api/auth/google/', GoogleLogin.as_view()),
     path('api/auth/facebook/', FacebookLogin.as_view()),
     path('api/articles/', include('articles.urls')),
